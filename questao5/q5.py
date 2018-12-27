f969e6b6fc330604a86eea8f3f2273d98ef29aa0
Asafe Oliveira <asafepy@gmail.com>
2018-12-27 03:10:40 -0200

    ajustando pep8

--- questao5/q5.py
+++ questao5/q5.py
@@ -1,16 +1,19 @@
 import json
 from pprint import pprint
 
+
 class FindKey:
 
     def __init__(self, file, key):
         self.file = file
         self.key = key
-    
+
     def _finditem(self, obj, key):
         # import pdb; pdb.set_trace()
-        if key in obj: return obj[key]
-        elif key.title() in obj: return obj[key.title()]
+        if key in obj:
+            return obj[key]
+        elif key.title() in obj:
+            return obj[key.title()]
 
         for k, v in obj.items():
             if isinstance(v, dict):
@@ -20,14 +23,12 @@ class FindKey:
     def get_value(self):
         print(self._finditem(self.file, self.key))
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 
     with open('files/pessoa.json') as f:
         data = json.load(f)
-    
+
     key = "age"
-
     results = FindKey(data, key)
-
     print(results.get_value)
